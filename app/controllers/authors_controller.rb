b0018b1c8fac826bc20067c524b43a70b68572cf
Teerth Patel <teerth1205@gmail.com>
2020-03-31 21:55:11 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,11 +8,30 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :new
+    end 
   end
-
+  
+  def edit 
+    @author = Author.find(params[:id])
+    render :edit
+  end 
+  
+  def update
+    @post = Post.find(params[:id])
+     if @post.update(author_params) 
+       redirect_to author_path(@author)
+     else 
+       render :edit
+     end 
+  end 
+  
   private
 
   def author_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :author,  only: [:new, :create, :show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
